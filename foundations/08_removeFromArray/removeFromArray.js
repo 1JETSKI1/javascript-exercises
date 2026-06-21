5c4106bc0c5825d26fc9f12dd3fb9d84a4f144a3
1JETSKI1 <261773881+1JETSKI1@users.noreply.github.com>
2026-06-21 19:13:12 +0200

    finished exercise remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+
+    });
+
+    return newArray
+
 };
+
+
+console.log(1, 2, 3, 4, 5);
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+i
