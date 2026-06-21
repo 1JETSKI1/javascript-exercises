eb052b25128f4a79a4a31ab8b429c92b69260eef
1JETSKI1 <261773881+1JETSKI1@users.noreply.github.com>
2026-06-21 22:47:41 +0200

    finished exercise tempconversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 9/5) + 32) * 10) / 10;
 };
+
+console.log(convertToCelsius(100));     // expect 37.8
+console.log(convertToFahrenheit(73.2)); // expect 163.8
+console.log(convertToCelsius(-100));    // expect -73.3
+console.log(convertToFahrenheit(-10));  // expect 14
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
